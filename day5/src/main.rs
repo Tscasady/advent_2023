d6fa85bdf55abebf3feb828a481db9f8082094c9
Thomas Casady <tscasady@gmail.com>
2023-12-05 19:44:18 -0600

    d5p1: complete

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::ops::Range;
 
 fn main() {
-    let file_path = "./src/test.txt";
+    let file_path = "./input.txt";
     let content = fs::read_to_string(file_path).unwrap();
     let chunks: Vec<Vec<&str>> = content
         .split("\n\n")
@@ -14,19 +14,18 @@ fn main() {
         .flat_map(|s| s.split_whitespace())
         .filter_map(|seed_data| seed_data.parse().ok())
         .collect();
-    let maps: Vec<Vec<Map>> = chunks[1..]
+    let maptypes: Vec<Vec<Map>> = chunks[1..]
         .iter()
         .map(|chunk| chunk[1..].iter().map(|s| Map::new(s)).collect())
         .collect();
     let mut min_location = i64::MAX;
     for seed in seeds {
-        for map in maps {
-            min_location = min(min_location, get_location(seed))
-        }
+        min_location = min(min_location, get_location(seed, &maptypes))
     }
     println!("{:?}", min_location)
 }
 
+#[derive(Debug)]
 struct Map {
     range: Range<i64>,
     start: i64,
@@ -39,12 +38,21 @@ impl Map {
             .map(|s| s.parse::<i64>().unwrap())
             .collect();
         Map {
-            range: (numbers[1]..numbers[1] + numbers[2]),
+            range: (numbers[1]..(numbers[1] + numbers[2])),
             start: numbers[0],
         }
     }
 }
 
-fn get_location(seed: i64) -> i64 {
-    todo!()
+fn get_location(seed: i64, maptypes: &Vec<Vec<Map>>) -> i64 {
+    let mut temp = seed;
+    for maps in maptypes {
+        for map in maps {
+            if map.range.contains(&temp) {
+                temp = map.start + (temp - map.range.start);
+                break;
+            }
+        }
+    }
+    temp
 }
